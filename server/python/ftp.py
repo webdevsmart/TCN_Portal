ce2b71ebf703628b1ad06a5fffc7ecdcf23bddbf
webdevsmart <webdevsmart@hotmail.com>
2021-06-05 10:40:06 +1000

    for cron

--- server/python/ftp.py
+++ server/python/ftp.py
@@ -8,16 +8,10 @@ import os
 def main():
 	files = []
 
-	try:
-		files = ftp.nlst()
-	except ftplib.error_perm, resp:
-		if str(resp) == "550 No files found":
-			print "No files in this directory"
-		else:
-			raise
+	files = ftp.nlst()
 
 	for f in files:
-		print f
+		print (f)
 
 	# print("*"*50, "LIST", "*"*50)
 	# ftp.dir()
