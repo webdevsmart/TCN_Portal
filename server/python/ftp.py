e486a254164081d6e277200d5955fa9e5cf5571d
webdevsmart <webdevsmart@hotmail.com>
2021-06-05 10:18:43 +1000

    update for cron

--- server/python/ftp.py
+++ server/python/ftp.py
@@ -6,11 +6,18 @@ ftp.cwd("vgc2")
 import os
 
 def main():
-	files = ftp.dir()
-	for dir in files:
+	files = []
 
-		if os.path.isdir(dir):
-			print(dir)
+	try:
+		files = ftp.nlst()
+	except ftplib.error_perm, resp:
+		if str(resp) == "550 No files found":
+			print "No files in this directory"
+		else:
+			raise
+
+	for f in files:
+		print f
 
 	# print("*"*50, "LIST", "*"*50)
 	# ftp.dir()
