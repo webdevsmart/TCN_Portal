4c8dd71a930a235af5ae4dc4c43ab04c2114187a
webdevsmart <webdevsmart@hotmail.com>
2021-06-23 12:26:02 +1000

    update for removing final

--- server/constants.js
+++ server/constants.js
@@ -31,7 +31,7 @@ const NO = 'No';
 const IMAGE_UPLOAD_URL = '/uploads/images/';
 // const LOG_FILE_PATH = "D:/Workspace/0514_vending_portal/vgc2_backup/";
 const LOG_FILE_PATH = "/home/vending/vgc2_backup/";
-const PYTHON_PATH = "/home/vending/test";
+const PYTHON_PATH = "/home/vending/python";
 // const PYTHON_PATH = "D:/Workspace/0514_vending_portal/test";
 
 module.exports = { 
