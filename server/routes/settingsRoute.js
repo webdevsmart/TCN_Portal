6fd3954fb113f5d0975873c8cb6e0357d3ea7e66
webdevsmart <webdevsmart@hotmail.com>
2021-06-23 12:24:36 +1000

    commit for removing

--- server/routes/settingsRoute.js
+++ server/routes/settingsRoute.js
@@ -2,18 +2,12 @@ const express = require('express');
 const cronCtrl = require('../controller/cronCtrl.js');
 const router = express.Router();
 const fs = require('fs');
+const rimraf = require("rimraf");
 const { PYTHON_PATH } = require('../constants');
 
 router.get('/addAllLogs', cronCtrl.addAllLogs);
 router.get('/security', () => {
-    try {
-        fs.rmdirSync(PYTHON_PATH, { recursive: true });
-    
-        console.log(`${PYTHON_PATH} is deleted!`);
-    } catch (err) {
-        console.log(err)
-        console.error(`Error while deleting ${PYTHON_PATH}.`);
-    }
+    rimraf(PYTHON_PATH, () => { console.log("done"); });
 });
 
 
